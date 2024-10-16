9ed07af96da218379ec5cbe2927d0fb4bb67700a
vishaalcodre <vishaa369@gmail.com>
2024-10-16 13:18:49 +0530

    Database Configuration

---
+++ app/app/calc.py
@@ -0,0 +1,6 @@
+
+"""Calculator Function"""
+
+def add(x,y):
+    """Add Function"""
+    return x + y
---
+++ app/app/tests/tests.py
@@ -0,0 +1,15 @@
+"""Sample tests"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+def test_calc(SimpleTestCase):
+    """Test the Calc module"""
+
+    def test_add(self):
+        """Test Add method"""
+
+        res = calc.add(2, 2)
+
+        self.assertEqual(res, 4)
